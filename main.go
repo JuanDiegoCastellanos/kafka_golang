78ad37fc10aceceefe38f0ffbe249993bb92189a
Juan Diego Castellanos Jerez <juandiego.castellanosjerez@gmail.com>
2024-01-26 14:40:59 -0500

    dependency and project initialization already

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "host1:9092,host2:9092",
+		"client.id":         socket.gethostname(),
+		"acks":              "all"})
+
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+		os.Exit(1)
+	}
+}
