0fa5f53350c99834b4f918cb4272e72acd2ca2f9
Juan Diego Castellanos Jerez <juandiego.castellanosjerez@gmail.com>
2024-01-26 17:44:44 -0500

    mergeing my own kafka with kafka external services

--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
-		"bootstrap.servers": "host1:9092,host2:9092",
-		"client.id":         socket.gethostname(),
+		"bootstrap.servers": "localhost:9092",
+		"client.id":         "something",
 		"acks":              "all"})
 
 	if err != nil {
 		fmt.Printf("Failed to create producer: %s\n", err)
-		os.Exit(1)
+
 	}
+	fmt.Printf("%+v\n", p)
 }
