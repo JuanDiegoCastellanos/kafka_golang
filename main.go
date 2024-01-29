079206637361e8cf491b6d3b69d976be67fbb40b
Juan Diego Castellanos Jerez <juandiego.castellanosjerez@gmail.com>
2024-01-29 17:33:05 -0500

    simple consuming and producing done

--- main.go
+++ main.go
@@ -2,32 +2,64 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 	"log"
+	"time"
+
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 )
 
 func main() {
+	topic := "HVSE"
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost:9092",
-		"client.id":         "something",
+		"client.id":         "foo",
 		"acks":              "all"})
 
 	if err != nil {
 		fmt.Printf("Failed to create producer: %s\n", err)
 
 	}
-	deliverych := make(chan kafka.Event, 10000)
-	topic := "HVSE"
-	err = p.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-		Value:          []byte("FOO")},
-		deliverych,
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+			"bootstrap.servers": "localhost:9092",
+			"group.id":          "foo",
+			"auto.offset.reset": "smallest"})
 
-	e := <-deliverych
-	fmt.Printf("%+v\n", e.String())
-	fmt.Printf("%+v\n", p)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = consumer.Subscribe(topic, nil)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			ev := consumer.Poll(100)
+			switch e := ev.(type) {
+			case *kafka.Message:
+				fmt.Printf("consumed message from the queue:  %s \n", string(e.Value))
+			case *kafka.Error:
+				fmt.Printf("%s \n", e)
+			}
+		}
+
+	}()
+
+	deliverych := make(chan kafka.Event, 10000)
+
+	for {
+
+		err = p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte("FOO")},
+			deliverych,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		<-deliverych
+		time.Sleep(time.Second * 3)
+	}
 }
